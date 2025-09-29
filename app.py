5087852b6f36d032bb1f065b5640f1fa5a121720
luisalvs <luisalvst@gmail.com>
2025-09-29 17:03:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,7 +18,25 @@ class Product(db.Model):
 @app.route('/api/products/add', methods=['POST'])
 def add_product():
     data = request.json
-    return data
+    if 'name' in data and 'price' in data:
+        product = Product(name=data['name'], price=data['price'],
+                          # model do produto, atributo e nome do produto
+                          description=data.get('description', ''))
+        db.session.add(product)  # adicionando produto ao db
+        db.session.commit()
+        return jsonify({'message': 'Product added successfully'})
+    return jsonify({'message': 'Invalid product data'}), 400
+
+
+@app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    # recuperar o produta da base de dados
+    product = Product.query.get(product_id)
+    if product != None:
+        db.session.delete(product)
+        db.session.commit()  # para realizar a mudança na base
+        return jsonify({'message': 'Product deleted successfully'})
+    return jsonify({'message': 'Product not found'}), 404
 
 
 if __name__ == '__main__':
