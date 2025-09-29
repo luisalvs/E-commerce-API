5964dad050dc5d6408e498f631522bd54f5762d6
luisalvs <luisalvst@gmail.com>
2025-09-29 20:23:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,18 @@ def delete_product(product_id):
     return jsonify({'message': 'Product not found'}), 404
 
 
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def get_products_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'description': product.description
+        })
+    return jsonify({'message': 'Product not found'}), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
