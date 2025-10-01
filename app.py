3258960d154b862368a8392a79d6e935c5815817
luisalvs <luisalvst@gmail.com>
2025-09-30 22:29:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'minha_chave_123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 CORS(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
 
 class Product(db.Model):
@@ -17,7 +28,32 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=True)
 
 
+# Autenticação
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    user = User.query.filter_by(username=data.get('username')).first()
+    if user and data.get('password') == user.password:
+        login_user(user)
+        return jsonify({'message': 'Logged in successfully'})
+
+    return jsonify({'message': 'Unauthorized. Invalid credentials'}), 401
+
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logout successfully'})
+
+
 @app.route('/api/products/add', methods=['POST'])
+@login_required
 def add_product():
     data = request.json
     if 'name' in data and 'price' in data:
@@ -31,7 +67,9 @@ def add_product():
 
 
 @app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
+@login_required  # protegendo rota com autenticação
 def delete_product(product_id):
+
     # recuperar o produta da base de dados
     product = Product.query.get(product_id)
     if product != None:
@@ -55,6 +93,7 @@ def get_products_details(product_id):
 
 
 @app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+@login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
